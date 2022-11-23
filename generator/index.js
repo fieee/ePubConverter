a9c2ff04b25cee0f0bc23b5eefb1014574ee1b07
zhiyi xu <zhiyi.xu@rwth-aachen.de>
2022-11-23 23:05:31 +0100

    manual_epub_generator

---
+++ generator/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const epub = require('epub-gen');
+
+const options = {
+  title: 'Moby-Dick',
+  author: 'Herman Melville',
+  output: './moby-dick.epub',
+  content: [
+    {
+      title: 'Chapter 1: Loomings',
+      data: `<p>
+        Call me Ishmael. Some years ago—never mind how long precisely
+      </p>`
+    }
+  ]
+};
+
+new epub(options).promise.then(() => console.log('Done'));
