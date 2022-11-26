3c82bc303cbd09c3179f33f6eb09d06b4c8f9dc0
Jixian Wang <jixian@Wangs-MacBook-Pro.local>
2022-11-26 17:09:54 +0200

    add ppub sample

--- src/libs/createPageSMIL.js
+++ src/libs/createPageSMIL.js
@@ -1,3 +1,26 @@
-module.exports = async () => {
+const fs = require('fs');
+const path = require('path');
+const copy = require('recursive-copy');
+const ejs = require('ejs-promise');
 
+module.exports = async (page, options) => {
+  // create xhtml page
+  const fileRoot = path.join(options.epubPath, 'OBEPS');
+  const filename = path.join(fileRoot, 'smil', `page-${page.id}.smil`);
+
+  const data = {
+    ...page,
+    audioFile: path.join('audio', path.basename(page.audioFile)),
+  }
+
+  // using ejs template to generate the page contten
+  const templateFile = path.normalize(path.join(path.dirname(__dirname), 'templates','content','page.smil.ejs'));
+  const templateData = fs.readFileSync(templateFile, 'utf-8');
+  const renderedData = await ejs.render(templateData, data, { rmWhitespace: true});
+
+  // write page file
+  fs.writeFileSync(filename, renderedData);
+  if(process.env.VERBOSE) console.log('ePub smil file generated = ', filename);
+
+  return filename;
 }
