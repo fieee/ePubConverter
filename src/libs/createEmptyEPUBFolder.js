68a5f4c3b53af28a0c806c959c049ce039c52b8e
Jixian Wang <jixian@Wangs-MacBook-Pro.local>
2022-11-26 14:37:40 +0200

    finished create empty epub feature

--- src/libs/createEmptyEPUBFolder.js
+++ src/libs/createEmptyEPUBFolder.js
@@ -1,8 +1,14 @@
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
+const copy = require('recursive-copy');
 
 module.exports = async () => {
   const epubPath = fs.mkdtempSync(path.join(os.tmpdir(), 'epub-'));
   if(process.env.VERBOSE) console.log('ePub temp folder = ', epubPath);
+  //copy empty template to the dst folde
+  const templateFolder = path.normalize(path.dirname(__dirname) + '/templates/epub3');
+  await copy(templateFolder, epubPath);
+
+  return epubPath;
 }
