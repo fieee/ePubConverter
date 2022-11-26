af71b57402e0e90ba44b32725e16456cb0177674
Jixian Wang <jixian@Wangs-MacBook-Pro.local>
2022-11-26 14:12:00 +0200

    finished ppub json parser

--- src/libs/parsepPPubData.js
+++ src/libs/parsepPPubData.js
@@ -1,5 +1,10 @@
-module.exports = async () => {
-  // Code here
-  console.log('parsePPubData')
-  return { pages: [] }
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async (bookFolder) => {
+  const dataFile  = path.join(bookFolder,'data/book.json');
+  if (!fs.existsSync(dataFile)) throw new Error("book.json data file does not exist");
+  const bookData = JSON.parse(fs.readFileSync(dataFile, {encoding: 'utf-8'}));
+  if(process.env.VERBOSE) console.log('pages loaded = ', bookData.pages.length);
+  return bookData;
 }
