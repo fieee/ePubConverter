a3ba22f94dce01e43f8e24a466f5471fb5f48cb3
Jixian Wang <jixian@Wangs-MacBook-Pro.local>
2022-11-26 14:29:10 +0200

    add empty epub3 template

--- src/libs/createEmptyEPUBFolder.js
+++ src/libs/createEmptyEPUBFolder.js
@@ -1,4 +1,8 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
 module.exports = async () => {
-  // Code here
-  console.log('Empty ePub created')
+  const epubPath = fs.mkdtempSync(path.join(os.tmpdir(), 'epub-'));
+  if(process.env.VERBOSE) console.log('ePub temp folder = ', epubPath);
 }
